9b4885350d6bde6622739ab24c57eb55f7917590
bbbsavych <bsavych@perpet.io>
2016-06-05 20:30:37 +0300

    Added slider for future purpose.

--- PhotoEditing/PhotoEditing/Controllers/ImageController/ImageDemonstrateController.m
+++ PhotoEditing/PhotoEditing/Controllers/ImageController/ImageDemonstrateController.m
@@ -16,14 +16,19 @@
 
 @property (nonatomic, weak) IBOutlet UIImageView *currentImageView;
 @property (nonatomic, weak) IBOutlet UICollectionView *filterCollectionView;
+@property (nonatomic, weak) UISlider *slider;
 @property (nonatomic, strong) NSArray<NSString *> *filtersNameDataSource;
 @property (nonatomic, weak) IBOutlet UIActivityIndicatorView *activityIndicator;
 @property (nonatomic, strong) UIImage *backupImage;
 
 @end
 
+#define DEGREES_TO_RADIANS(angle) ((angle) / 180.0 * M_PI)
+
 static const CGFloat kCollectionViewInset = 10.0;
 static const CGFloat kCollectionViewCellWidth = 110.f;
+static const CGFloat kSliderRightOffset = 20.f;
+static const CGFloat kSliderConstantWidht = 31.f;
 
 @implementation ImageDemonstrateController
 
@@ -37,6 +42,7 @@ static const CGFloat kCollectionViewCellWidth = 110.f;
     [super viewDidLoad];
 
     [self prepareViewController];
+    [self createSlider];
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -68,6 +74,8 @@ static const CGFloat kCollectionViewCellWidth = 110.f;
     
     [moreDetailsAlertController addAction:[UIAlertAction actionWithTitle:NSLocalizedString(@"Back to original image", nil) style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
         weakself.currentImageView.image = weakself.backupImage;
+        weakself.currentImage = weakself.backupImage;
+        [weakself.filterCollectionView reloadData];
     }]];
     [moreDetailsAlertController addAction:[UIAlertAction actionWithTitle:NSLocalizedString(@"Cancel", nil) style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
         [weakself dismissViewControllerAnimated:YES completion:nil];
@@ -170,6 +178,18 @@ static const CGFloat kCollectionViewCellWidth = 110.f;
         NSString *result = error ? @"Not saved." : @"Succesfully saved";
         [UIAlertView showWithTitle:@"Result" message:NSLocalizedString(result, nil) handler:nil];
     }];
+}
+
+- (void)createSlider
+{
+    UISlider *slider = [[UISlider alloc] initWithFrame:CGRectMake(CGRectGetMidX(self.view.frame), CGRectGetMidY(self.view.frame), CGRectGetHeight(self.currentImageView.frame) / 2.f, kSliderConstantWidht)];
+    slider.transform = CGAffineTransformMakeRotation(DEGREES_TO_RADIANS(90));
+    [self.view addSubview:slider];
+    self.slider = slider;
+    CGFloat div = self.view.frame.size.width - self.slider.center.x;
+    
+    if (div > kSliderRightOffset)
+        self.slider.center = CGPointMake(self.slider.center.x + div - kSliderRightOffset, self.slider.center.y);
 }
 
 @end
